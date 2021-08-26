64490e79e17d899d22e56c282bb72ba376d545b7
cross-country <landaralex2@gmail.com>
2021-08-26 23:06:48 +0300

    Static_Method is finished

--- OOP/Static_Method.py
+++ OOP/Static_Method.py
@@ -11,19 +11,19 @@ class Cylinder:
     def __init__(self, diameter, height):
         self.dia = diameter
         self.h = height
-        self.__area = self.make_area(self.dia, self.h)
 
     def __setattr__(self, attr, value):
-        #self.__area = self.make_area(self.dia, self.h)
         self.__dict__[attr] = value
+        if 'dia' in self.__dict__ and 'h' in self.__dict__:
+            self.__dict__['_area'] = self.make_area(self.dia, self.h)
+
 
     def getErea(self):
-        print(self.__area)
+        print(self._area)
 
 
 a = Cylinder(2, 2)
 a.h = 5
-print(a.h)
 a.getErea()
 
 #print(a.make_area(2, 2))
--- tkinter/try2.py
+++
@@ -1,5 +0,0 @@
-import sys, warnings, os, platform, logging
-
-print(sys.version_info)
-warnings.warn('hello', RuntimeWarning)
-print()
