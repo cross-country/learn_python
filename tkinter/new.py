37d5c603a160b10e0b09480d36b9487ffd185427
cross-country <landaralex2@gmail.com>
2021-12-13 22:33:30 +0200

    Delete tkinter directory

--- tkinter/new.py
+++
@@ -1,9 +0,0 @@
-try:
-    a = 100
-    while a >= 0:
-        b = int(input("Введите число: "))
-        a = a - b
-    print("недопустимое число")
-    print(a)
-except ValueError:
-    print("Введите число!")
